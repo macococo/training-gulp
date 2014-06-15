095185e40e796b7d216b178136c2cebed8d16eaa
Makoto Kudo <silver.tabby.0620@gmail.com>
2014-06-15 17:54:24 +0900

    add clean task

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,34 +2,39 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   minifyCss = require('gulp-minify-css'),
-  imagemin = require('gulp-imagemin');
+  imagemin = require('gulp-imagemin'),
+  rimraf = require('rimraf');
 
 var path = {
   js: ['../vendor/**/*.js'],
   css: ['../vendor/**/*.css']
 }
 
-gulp.task('bench-concat', function() {
+gulp.task('clean', function(cb){
+  rimraf('dist/', cb);
+});
+
+gulp.task('bench-concat', ['clean'], function() {
   gulp.src(path.js)
     .pipe(concat('all.js'))
     .pipe(gulp.dest('dist/js/'));
 });
 
-gulp.task('bench-uglify', function() {
+gulp.task('bench-uglify', ['clean'], function() {
   gulp.src(path.js)
-    .pipe(uglify())
     .pipe(concat('all.min.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
 });
 
-gulp.task('bench-cssmin', function() {
-  gulp.src(path.css, {buffer: false})
-    .pipe(concat('all.min.css'))
+gulp.task('bench-cssmin', ['clean'], function() {
+  gulp.src(path.css)
     .pipe(minifyCss())
+    .pipe(concat('all.min.css'))
     .pipe(gulp.dest('dist/css/'));
 });
 
-gulp.task('bench-imagemin', function() {
+gulp.task('bench-imagemin', ['clean'], function() {
   gulp.src(['../images/**/*.{png,jpg,gif}'])
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images/'));
